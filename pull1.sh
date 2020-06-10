8cc44b1a1d4815d7c3f7e30f9b32cbe727af15b1
willgithub <524869004@qq.com>
2020-06-10 23:08:13 +0800

    Create pull1.sh

---
+++ pull1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+icount=1
+images=$(sed -n '1,50p' images.txt)
+for image in $images
+do
+  echo [$icount/$count]: $image
+  docker pull $DOCKER_NAMESPACE/${KOLLA_BASE_DISTRO}-${INSTALL_TYPE}-$image:$TAG
+  docker tag $DOCKER_NAMESPACE/${KOLLA_BASE_DISTRO}-${INSTALL_TYPE}-$image:$TAG $ALIYUN_REGISTRY/$ALIYUN_NAMESPACE/${KOLLA_BASE_DISTRO}-${INSTALL_TYPE}-$image:$TAG
+  docker push $ALIYUN_REGISTRY/$ALIYUN_NAMESPACE/${KOLLA_BASE_DISTRO}-${INSTALL_TYPE}-$image:$TAG
+  ((icount++))
+done
