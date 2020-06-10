1b7b13395996e0fb9966fbd157e8a0e79685c3bb
willgithub <524869004@qq.com>
2020-06-10 11:22:14 +0800

    Update get_kolla_images_list.sh

--- get_kolla_images_list.sh
+++ get_kolla_images_list.sh
@@ -2,13 +2,13 @@
 
 git clone https://github.com/openstack/kolla.git
 
-first_index=($(ls -l kolla/docker | grep "^d" | awk '{print $NF}'))
+first_index=`$(ls -l kolla/docker | grep "^d" | awk '{print $NF}')`
 
 function get_kolla_image(){
   for first_file in ${first_index[@]}
     do
     #echo $first_file
-    second_files=($(ls -l kolla/docker/$first_file | grep "^d" | awk '{print $NF}'))
+    second_files=`$(ls -l kolla/docker/$first_file | grep "^d" | awk '{print $NF}')`
     #判断是否存在二级目录
     if [ ${#second_files[@]} -eq 0 ]
     then
