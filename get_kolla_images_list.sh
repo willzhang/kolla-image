5732c9c8888844901499575cbc38285619285e2b
willgithub <524869004@qq.com>
2020-06-10 11:35:22 +0800

    Update get_kolla_images_list.sh

--- get_kolla_images_list.sh
+++ get_kolla_images_list.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-set -x
 git clone https://github.com/openstack/kolla.git
 
-first_index=(`ls -l kolla/docker | grep "^d" | awk '{print $NF}'`)
+first_index=($(ls -l kolla/docker | grep "^d" | awk '{print $NF}'))
 
 function get_kolla_image(){
   for first_file in ${first_index[@]}
     do
-    #echo $first_file
-    second_files=(`ls -l kolla/docker/$first_file | grep "^d" | awk '{print $NF}'`)
-    #判断是否存在二级目录
+    #check if subdirectory exits
+    second_files=($(ls -l kolla/docker/$first_file | grep "^d" | awk '{print $NF}'))
     if [ ${#second_files[@]} -eq 0 ]
     then
       echo $first_file >> images.txt
